533370570cdc2123a06eb57e3c91c776438b4256
Mike-Dai <mrmikedai@outlook.com>
2019-04-26 12:46:50 +0800

    Finish Q11 and Q12

--- hw05/hw05.py
+++ hw05/hw05.py
@@ -536,10 +536,12 @@ def interval(a, b):
 def lower_bound(x):
     """Return the lower bound of interval x."""
     "*** YOUR CODE HERE ***"
+    return x[0]
 
 def upper_bound(x):
     """Return the upper bound of interval x."""
     "*** YOUR CODE HERE ***"
+    return x[1]
 
 def str_interval(x):
     """Return a string representation of interval x."""
@@ -555,16 +557,20 @@ def add_interval(x, y):
 def mul_interval(x, y):
     """Return the interval that contains the product of any value in x and any
     value in y."""
-    p1 = x[0] * y[0]
-    p2 = x[0] * y[1]
-    p3 = x[1] * y[0]
-    p4 = x[1] * y[1]
-    return [min(p1, p2, p3, p4), max(p1, p2, p3, p4)]
+    p1 = lower_bound(x) * lower_bound(y)
+    p2 = lower_bound(x) * upper_bound(y)
+    p3 = upper_bound(x) * lower_bound(y)
+    p4 = upper_bound(x) * upper_bound(y)
+    return interval(min(p1, p2, p3, p4), max(p1, p2, p3, p4))
 
 def sub_interval(x, y):
     """Return the interval that contains the difference between any value in x
     and any value in y."""
     "*** YOUR CODE HERE ***"
+    tempx = interval(lower_bound(x), upper_bound(x))
+    tempy = interval(-upper_bound(y), -lower_bound(y))
+    return add_interval(tempx, tempy)
+
 
 def div_interval(x, y):
     """Return the interval that contains the quotient of any value in x divided by

fbd3f80d050a0ab4615b063dd3b28ca4136988ce
Mike-Dai <mrmikedai@outlook.com>
2019-05-10 17:36:07 +0800

    Finish Q1 to Q3

--- hw08/hw08.scm
+++ hw08/hw08.scm
@@ -2,21 +2,29 @@
   (cdr (cdr s)))
 
 (define (cadr s)
-  'YOUR-CODE-HERE
+  (car (cdr s))
 )
 
 (define (caddr s)
-  'YOUR-CODE-HERE
+  (car (cddr s))
 )
 
 (define (sign x)
-  'YOUR-CODE-HERE
+  (cond ((< x 0) -1)
+        ((= x 0) 0)
+        ((> x 0) 1)
+  )
 )
 
 (define (square x) (* x x))
 
 (define (pow b n)
-  'YOUR-CODE-HERE
+  (cond 
+        ((= n 1) b)
+        ((= n 0) 1)
+        ((even? n) (square (pow b (/ n 2))))
+        ((odd? n) (* b (square (pow b (/ (- n 1) 2)))))
+  )
 )
 
 (define (ordered? s)

14d79f4a4523f7ed251bc11f9ac57dd171272d48
Mike-Dai <mrmikedai@outlook.com>
2019-05-17 12:19:22 +0800

    Finish hw10

--- hw10/hw10.scm
+++ hw10/hw10.scm
@@ -1,14 +1,17 @@
 (define (accumulate combiner start n term)
   (if (= n 0)
       start
-      'YOUR-CODE-HERE
+      (accumulate combiner (combiner start (term n)) (- n 1) term)
   )
 )
 
 (define (accumulate-tail combiner start n term)
-  'YOUR-CODE-HERE
-)
+  (define (helper n result)
+  	(if (= n 0)
+  		result
+  		(helper (- n 1) (combiner (term n) result))))
+  (helper n start))
 
 (define-macro (list-of expr for var in seq if filter-fn)
-  'YOUR-CODE-HERE
+  `(map (lambda (,var) ,expr) (filter (lambda (,var) ,filter-fn) ,seq))
 )

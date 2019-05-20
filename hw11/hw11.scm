27c28a11bc35b1a9bde09764e01ceb3083478ff8
Mike-Dai <mrmikedai@outlook.com>
2019-05-20 18:55:47 +0800

    Start hw11. Finish find

---
+++ hw11/hw11.scm
@@ -0,0 +1,18 @@
+(define (find s predicate)
+  (cond 
+  	((null? s) #f)
+  	((predicate (car s)) (car s))
+  	(else (find (cdr-stream s) predicate))
+  )
+)
+
+(define (scale-stream s k)
+  'YOUR-CODE-HERE
+)
+
+(define (has-cycle s)
+  'YOUR-CODE-HERE
+)
+(define (has-cycle-constant s)
+  'YOUR-CODE-HERE
+)
